7f0493a85ec5a7988ca6005b0f4699727c83d2b6
mhullas <mhullas07@gmail.com>
2023-12-12 22:11:50 +0600

    Admin Dashboard login Successfull.....

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
-<h1 class="text-center" style="margin-top: 15%">
+<h1 style="margin-left:40%; margin-top:20%">
     Welcome <span style="color:green">{{ $admin->name }}</span>
     <a style="color:crimson" href="{{ route('admin.logout') }}">Logout</a>
 </h1>
